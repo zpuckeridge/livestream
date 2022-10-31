2559618b9e7c7a7e08fa45cdb4bd3872b7bb3e67
zpuckeridge <zacchary@puckeridge.me>
2022-10-31 10:52:05 +0000

    Removed Sensitive Information

--- pages/api/cloudflare.ts
+++ pages/api/cloudflare.ts
@@ -1,14 +1,12 @@
-var CLOUDFLARE_ID = "0139d167327c252643c7691dc8b25c33";
-
 var cf = require("cloudflare")({
-  email: "zacchary@puckeridge.me",
-  key: "e5f80b8e52669c0ec2a2fc78eb54dde2fd0b8",
+  email: process.env.CLOUDFLARE_EMAIL,
+  key: process.env.CLOUDFLARE_TOKEN,
 });
 
 export default async function getVideos(req: any, res: any) {
   return new Promise<void>(async (resolve, reject) => {
     const data = await cf.stream
-      .listVideos(`${CLOUDFLARE_ID}`)
+      .listVideos(process.env.CLOUDFLARE_ID)
       .then((response: any) => {
         res.status(200);
         res.setHeader("Content-Type", "application/json");
