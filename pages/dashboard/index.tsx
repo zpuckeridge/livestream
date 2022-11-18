5677e80c5037f87078f3bf439560d8a680e1d3fe
zpuckeridge <zacchary@puckeridge.me>
2022-11-18 07:08:56 +0000

    feat: upload video ui

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { useSession, getSession } from "next-auth/react";
 import Link from "next/link";
-import { Upload } from "react-feather";
+import { Upload, UploadCloud, User } from "react-feather";
 
 export default function Admin() {
   const { data: session, status }: { data: any; status: any } = useSession();
@@ -13,11 +13,22 @@ export default function Admin() {
     return (
       <>
         <div className="xl:max-w-6xl mx-auto mt-10 mb-20">
-          <Link href="/dashboard/upload">
-            <button className="inline-flex mb-10 px-4 py-1 text-sm font-semibold rounded-full border bg-gray-200 dark:bg-[#111111] border-gray-200 dark:border-gray-700 hover:ring-2 ring-gray-300 transition-all hover:dark:bg-[#1d1f22] hover:border-transparent focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-2">
-              Upload Video <Upload className="ml-2" />
-            </button>
-          </Link>
+          <div className="justify-items-center text-center grid grid-cols-2 gap-4">
+            <div>
+              <Link href="/dashboard/profile">
+                <button className="p-10 rounded-lg flex items-center justify-center bg-gray-200 dark:bg-[#1d1f22] hover:ring-2 ring-gray-300  transition-all">
+                  Profile <User className="ml-2" />
+                </button>
+              </Link>
+            </div>
+            <div>
+              <Link href="/dashboard/upload">
+                <button className="p-10 rounded-lg flex items-center justify-center bg-gray-200 dark:bg-[#1d1f22] hover:ring-2 ring-gray-300  transition-all">
+                  Upload <UploadCloud className="ml-2" />
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </>
     );
--- pages/dashboard/upload.tsx
+++ pages/dashboard/upload.tsx
@@ -1,5 +1,6 @@
 import { useSession, getSession } from "next-auth/react";
 import Link from "next/link";
+import { Save } from "react-feather";
 import LoginButton from "../../components/login-btn";
 
 export default function Admin() {
@@ -33,12 +34,12 @@ export default function Admin() {
               <div className="mt-5 md:col-span-2 md:mt-0">
                 <form action="#" method="POST">
                   <div className="shadow sm:overflow-hidden sm:rounded-md">
-                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                    <div className="space-y-6 border border-gray-200 dark:border-[#333] rounded-xl px-4 py-5 sm:p-6">
                       <div className="grid grid-cols-3 gap-6">
                         <div className="col-span-3 sm:col-span-2">
                           <label
                             htmlFor="video-name"
-                            className="block text-sm font-medium text-black"
+                            className="block text-md font-bold text-black dark:text-white"
                           >
                             Name
                           </label>
@@ -47,7 +48,7 @@ export default function Admin() {
                               type="text"
                               name="video-name"
                               id="video-name"
-                              className="p-2 block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                              className="p-2 block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                               placeholder="Cool video #3000"
                             />
                           </div>
@@ -57,7 +58,7 @@ export default function Admin() {
                       <div>
                         <label
                           htmlFor="description"
-                          className="block text-sm font-medium text-black"
+                          className="block text-md font-bold text-black dark:text-white"
                         >
                           Description
                         </label>
@@ -76,25 +77,34 @@ export default function Admin() {
                         </p>
                       </div>
 
-                      <div>tags</div>
+                      <div>
+                        <label
+                          htmlFor="description"
+                          className="block text-md font-bold text-black dark:text-white"
+                        >
+                          Tags
+                        </label>
+                        <div className="flex mt-1 gap-2">
+                          <button className="px-4 py-1 text-sm font-semibold rounded-full border bg-gray-200 dark:bg-[#111111] border-gray-200 dark:border-gray-700 hover:ring-2 ring-gray-300 transition-all hover:dark:bg-[#1d1f22] hover:border-transparent focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-2">
+                            ARMA
+                          </button>
+                          <button className="px-4 py-1 text-sm font-semibold rounded-full border bg-gray-200 dark:bg-[#111111] border-gray-200 dark:border-gray-700 hover:ring-2 ring-gray-300 transition-all hover:dark:bg-[#1d1f22] hover:border-transparent focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-2">
+                            CSGO
+                          </button>
+                          <button className="px-4 py-1 text-sm font-semibold rounded-full border bg-gray-200 dark:bg-[#111111] border-gray-200 dark:border-gray-700 hover:ring-2 ring-gray-300 transition-all hover:dark:bg-[#1d1f22] hover:border-transparent focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-2">
+                            Other
+                          </button>
+                        </div>
+                      </div>
 
                       <div>
-                        <label className="block text-sm font-medium text-black">
+                        <label className="block text-md font-bold text-black dark:text-gray-200">
                           Thumbnail
                         </label>
                         <div className="mt-1 flex items-center">
-                          <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                            <svg
-                              className="h-full w-full text-gray-300"
-                              fill="currentColor"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                          </span>
                           <button
                             type="button"
-                            className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-black shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            className="rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-black shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                           >
                             Upload
                           </button>
@@ -102,7 +112,7 @@ export default function Admin() {
                       </div>
 
                       <div>
-                        <label className="block text-sm font-medium text-black">
+                        <label className="block text-md font-bold text-black dark:text-white">
                           Video Upload
                         </label>
                         <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
@@ -124,7 +134,7 @@ export default function Admin() {
                             <div className="flex text-sm text-black-600">
                               <label
                                 htmlFor="video-upload"
-                                className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                                className="relative cursor-pointer rounded-md font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
                               >
                                 <span>Upload a video</span>
                                 <input
@@ -142,14 +152,6 @@ export default function Admin() {
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                      <button
-                        type="submit"
-                        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        Save
-                      </button>
                     </div>
                   </div>
                 </form>
