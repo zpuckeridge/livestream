94ef8f519edc9d032cf25b6e4f217dd76ad2d5b6
zpuckeridge <zacchary@puckeridge.me>
2022-12-30 15:19:11 +0000

    fix: updated auth/discord login button method

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { FaDiscord } from "react-icons/fa";
 
 export default function LoginButton() {
   const { data: session }: { data: any } = useSession();
@@ -9,7 +11,12 @@ export default function LoginButton() {
           className="p-2 rounded-lg flex items-center justify-center bg-white dark:bg-white/5 border border-zinc-800/50 hover:ring-2 ring-gray-300 transition-all"
           onClick={() => signOut()}
         >
-          Sign out {session.user.email}
+          <img
+            src={session.user.image}
+            alt="user image"
+            className="rounded-full h-6 w-6 mr-2"
+          />
+          {session.user.name}
         </button>
       </>
     );
@@ -20,7 +27,8 @@ export default function LoginButton() {
         className="p-2 rounded-lg flex items-center justify-center bg-white dark:bg-white/5 border border-zinc-800/50 hover:ring-2 ring-gray-300 transition-all"
         onClick={() => signIn()}
       >
-        Sign in
+        <FaDiscord className="mr-2 h-6 w-6" />
+        Login
       </button>
     </>
   );
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export const authOptions = {
+export default NextAuth({
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
@@ -9,6 +9,4 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXT_AUTH_SECRET,
-};
-
-export default NextAuth(authOptions);
+});
