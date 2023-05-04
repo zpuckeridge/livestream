518e1da887df3123b800bc8dd2221583945464c7
zpuckeridge <zacchary@puckeridge.me>
2023-05-04 00:14:20 +0000

    fix: exposed and secured like increment route

--- pages/api/increment/[id].ts
+++ pages/api/increment/[id].ts
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const id = req.query.id as string;
+  const { userId } = getAuth(req);
 
-  await prisma.videos.updateMany({
-    where: { asset_id: id },
-    data: { likes: { increment: 1 } },
-  });
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  } else {
+    const id = req.query.id as string;
 
-  res.status(200).json({ message: "Likes updated" });
+    await prisma.videos.updateMany({
+      where: { asset_id: id },
+      data: { likes: { increment: 1 } },
+    });
+
+    res.status(200).json({ message: "Likes updated" });
+  }
 }
-
-// THIS ROUTE NEEDS TO BE PROTECTED - ONLY LOGGED IN USERS SHOULD HAVE ACCESS
-// ACCESS SHOULD BE LOGGED
