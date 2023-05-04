2a139f3c41eca5f214c30816b0a0c9d4e1b92b3a
zpuckeridge <zacchary@puckeridge.me>
2023-05-04 04:29:10 +0000

    fix: updated dynamic metadata

--- app/clip/[id]/page.tsx
+++ app/clip/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import type { Metadata } from "next";
 import prisma from "@/lib/prisma";
 import Player from "@/components/Player";
 import CopyLink from "@/components/CopyLink";
@@ -8,6 +8,44 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import Likes from "@/components/Likes";
 import Script from "next/script";
+
+export async function generateMetadata({ params }: any) {
+  const { id } = params;
+
+  const video = await prisma.videos.findUnique({
+    where: { asset_id: decodeURIComponent(id) },
+  });
+
+  return {
+    title: video?.title,
+    description: video?.description,
+    openGraph: {
+      title: video?.title,
+      description: video?.description,
+      url: `https://sdelta.xyz/clip/${video?.asset_id}`,
+      siteName: "sdelta.xyz",
+      images: [
+        {
+          url: `https://image.mux.com/${video?.playback_id}/thumbnail.png`,
+          width: 800,
+          height: 600,
+        },
+        {
+          url: `https://image.mux.com/${video?.playback_id}/thumbnail.png`,
+          width: 1800,
+          height: 1600,
+        },
+      ],
+      videos: {
+        url: `https://stream.mux.com/${video?.playback_id}/medium.mp4`,
+        width: 1280,
+        height: 720,
+      },
+      locale: "en-US",
+      type: "video.other",
+    },
+  };
+}
 
 export default async function Clip({ params }: any) {
   const { id } = params;
@@ -28,37 +66,6 @@ export default async function Clip({ params }: any) {
   return (
     <>
       <Script src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1" />
-      <Head>
-        <title>{video?.title}</title>
-        <meta name="description" content={video?.title} />
-        <meta property="og:title" content={video?.title} />
-        <meta property="og:site_name" content="sdelta.xyz" />
-        <meta property="og:type" content="video.other" />
-        <meta
-          property="og:image"
-          content={`https://image.mux.com/${video?.playback_id}/thumbnail.png`}
-        />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1280" />
-        <meta property="og:image:height" content="720" />
-        <meta
-          property="og:url"
-          content={`https://sdelta.xyz/clip/${video?.asset_id}`}
-        />
-        <meta
-          property="og:video"
-          content={`https://stream.mux.com/${video?.playback_id}/medium.mp4`}
-        />
-        <meta
-          property="og:video:url"
-          content={`https://stream.mux.com/${video?.playback_id}/medium.mp4`}
-        />
-        <meta property="og:video:type" content="text/html" />
-        <meta property="og:video:width" content="1280" />
-        <meta property="og:video:height" content="720" />
-        <meta property="og:video:release_date" content={`${video?.date}`} />
-        <meta property="og:video:duration" content={`${video?.duration}`} />
-      </Head>
       <main>
         <div className="max-w-6xl p-4 mx-auto">
           <Player playbackId={video?.playback_id} />
