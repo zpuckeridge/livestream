8d03f3b5a00729cf020609e961d43263f071133b
zpuckeridge <zacchary@puckeridge.me>
2023-05-23 05:10:58 +0000

    fix: refactor

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["cdn.discordapp.com", "image.mux.com"],
   },
